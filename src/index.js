14db7ed6d5e5d2da67389f47f35f5d2b10ed64ff
shengoo <shengqing@babytree-inc.com>
2018-06-03 18:06:12 +0800

    add image

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react'
+import image from './nophoto@2x.png';
 
 export default class extends Component {
   render() {
     return <div>
       <h2>Welcome to React components</h2>
+      <img src={image} />
     </div>
   }
 }
